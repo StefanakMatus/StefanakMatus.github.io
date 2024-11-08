75618d8215987aadca52fd088a1f23a4f7a9ca88
Matus Stefanak <matus.stefanak25@gmail.com>
2024-11-08 23:16:36 +0100

    slowly getting there

--- index.js
+++ index.js
@@ -41,8 +41,10 @@ function checkCorrect(){
 
     for (let i = 0; i < current_length; i++) {
         if (input[i] != text[i]) {
+            console.log("Wrong at: " + i);
             output += `<span class="wrong">${input[i]}</span>`;  // Incorrect characters
         } else {
+            console.log("Correct at: " + i);
             output += `<span class="correct">${input[i]}</span>`;  // Correct characters
         }
     }
