fe7ae76749c08df3fcac2a9270639c1719a59a73
Matus Stefanak <matus.stefanak25@gmail.com>
2024-11-08 21:46:51 +0100

    js into html

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 function whichKey(e){
     let charCode = e.keyCode
     console.log("Before: " + charCode);
+    const specialKeys = {
+        8: "Backspace",
+        9: "Tab",
+        13: "Enter",
+        16: "Shift",
+        17: "Control",
+        18: "Alt",
+        27: "Escape",
+        32: "Space",
+        37: "Arrow Left",
+        38: "Arrow Up",
+        39: "Arrow Right",
+        40: "Arrow Down"
+    };
+
+    // Check if the key is in the specialKeys object
+    if (specialKeys[charCode]) {
+        return specialKeys[charCode];
+    }
+
     return String.fromCharCode(charCode); 
 }
 
